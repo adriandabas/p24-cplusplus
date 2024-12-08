45edab7ce063cd56332869955e8a16e74a91b241
Adrian Dabas <adrian.dabas@etu.minesparis.psl.eu>
2024-12-08 09:56:55 +0100

    nouvelle fonction swap avec des references

--- swap.cpp
+++ swap.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 
-void swap(float* pf, float* pg) { //notre fonction prend en argument des ADRESSES MÉMOIRE, void signifie qu'elle ne renvoie rien
-    float t = (*pf);
+void swap_pointer(int* pf, int* pg) { //notre fonction prend en argument des ADRESSES MÉMOIRE, void signifie qu'elle ne renvoie rien
+    int t = (*pf);
     (*pf)=(*pg);
     (*pg)=t;
 }
 
+void swap_reference(int& f, int& g) {
+    int t=f;
+    f=g;
+    g=t;
+}
+
 int main() {
-    float f {7.24}; //équivalent à f=7.24
-    float g {}; //g=0
-    float* pf=&f; 
-    float* pg=&g;
-    swap(pf,pg);
-    std::cout << f << ' ' << g << std::endl;
+    int a {12}; 
+    int b {81};
+    //swap_pointer(&a,&b);
+    swap_reference(a,b);
+    std::cout << 'a' << '=' << a << " et " << 'b' << '=' << b << std::endl;
     return 0;
 }
